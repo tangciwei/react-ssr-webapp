9c0965c7fb1ec10170faad940601fc40a95e8aa6
tangciwei <tangciwei@baidu.com>
2018-10-16 07:56:16 +0800

    3-html+js配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: {
         app: path.join(__dirname, '../client/app.js')
@@ -13,7 +15,19 @@ module.exports = {
             {
                 test: /.jsx$/,
                 loader: 'babel-loader'
+            },
+            // 这里js和jsx要分开
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ]
+
             }
         ]
-    }
+    },
+    plugins: [
+        new HTMLPlugin()
+    ]
 };
